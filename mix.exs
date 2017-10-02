9d6b0b1f1a9ce103f2c0731bd100fd55f6e9dd01
Vyacheslav Voronchuk <voronchuk@gmail.com>
2017-10-02 18:39:56 +0300

    Bumped deps versions

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoGss.Mixfile do
     def project do
         [
             app: :ecto_gss,
-            version: "0.1.4",
+            version: "0.1.5",
             elixir: "~> 1.4",
             description: "Use Google Spreadsheets as storage for Ecto objects.",
             docs: [extras: ["README.md"]],
@@ -36,7 +36,7 @@ defmodule EctoGss.Mixfile do
 
     defp deps do
         [
-            {:elixir_google_spreadsheets, "~> 0.1.6"},
+            {:elixir_google_spreadsheets, "~> 0.1.8"},
             {:ecto, "~> 2.1"},
             {:earmark, ">= 0.0.0", only: :dev},
             {:ex_doc, ">= 0.0.0", only: :dev},
